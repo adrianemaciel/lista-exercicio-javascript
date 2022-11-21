c773cf0a719930938c69f23e74d0093b7120cbe1
adrianemaciel <adrianemaciel.developer@gmail.com>
2022-11-21 18:12:24 -0300

    exercicio02

--- exercicio_02.js
+++ exercicio_02.js
@@ -11,3 +11,21 @@
 // - Entre 25 e 30 Acima do peso;
 // - Entre 30 e 40 Obeso;
 // - Acima de 40 Obsesidade Grave;
+
+const peso = 80;
+const altura = 1.6;
+const imc = peso / Math.pow(altura, 2);
+console.log(imc);
+// biblioteca com funções matematicas - retorna a potencia ao quadrado da altura
+
+if (imc < 18.5) {
+  console.log("Abaixo do peso");
+} else if (imc >= 18.5 && imc < 25) {
+  console.log("Peso normal");
+} else if (imc >= 25 && imc < 30) {
+  console.log("Acima do peso");
+} else if (imc >= 30 && imc < 40) {
+  console.log("Obeso");
+} else {
+  console.log("Obesidade grave");
+}
